6ce21470c9c999120d8199614d03273dc4102421
tuminglong@126.com <T94m09l22>
2020-07-08 13:36:09 +0800

    网关优化

--- platform/fast-gateway-service/src/main/java/com/tml/gateway/FastGatewayApplication.java
+++ platform/fast-gateway-service/src/main/java/com/tml/gateway/FastGatewayApplication.java
@@ -1,13 +1,18 @@
 package com.tml.gateway;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
-@EnableAspectJAutoProxy
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @EnableDiscoveryClient
+@EnableFeignClients
+@RefreshScope
+@EnableAspectJAutoProxy
 public class FastGatewayApplication {
 
     public static void main(String[] args) {
