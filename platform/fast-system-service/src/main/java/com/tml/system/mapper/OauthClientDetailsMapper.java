4e204b93bf0e93b0367229b2d44782a148945d1f
JacksonTu <tuminglong@126.com>
2020-08-18 16:59:19 +0800

    初始化导入

--- platform/fast-system-service/src/main/java/com/tml/system/mapper/OauthClientDetailsMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tml.system.mapper;
-
-
-import com.tml.common.web.mapper.SuperMapper;
-import com.tml.system.entity.OauthClientDetails;
-
-public interface OauthClientDetailsMapper extends SuperMapper<OauthClientDetails> {
-}
